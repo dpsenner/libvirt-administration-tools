06504fcfb9d0062549ebfd04484209a6548bd549
Dominik Psenner <dpsenner@gmail.com>
2017-10-12 21:55:44 +0200

    Add support to retry blockcommit up to 5 times

--- vm-live-backup.sh
+++ vm-live-backup.sh
@@ -6,11 +6,13 @@
 DOMAIN="$1"
 BACKUPDEST="$2"
 
-if [ -z "$BACKUPDEST" ]; then
+if [ -z "$BACKUPDEST" ]
+then
     BACKUPDEST="/var/data/virtuals/backups"
 fi
 
-if [ -z "$BACKUPDEST" -o -z "$DOMAIN" ]; then
+if [ -z "$BACKUPDEST" -o -z "$DOMAIN" ]
+then
     echo "Usage: ./vm-live-backup <domain> [backup-folder]"
     exit 1
 fi
@@ -20,7 +22,8 @@ fi
 #
 SCRIPT_PATH="`dirname \"$0\"`"              # relative
 SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"  # absolutized and normalized
-if [ -z "$SCRIPT_PATH" ] ; then
+if [ -z "$SCRIPT_PATH" ]
+then
 	# error; for some reason, the path is not accessible
 	# to the script (e.g. permissions re-evaled after suid)
 	exit 1  # fail
@@ -44,8 +47,10 @@ IMAGES=`virsh domblklist "$DOMAIN" --details | grep "^file[[:space:]]*disk" | aw
 # Check if another backup is still in progress
 # This is the case when teh disk contains the pattern snapshot
 # 
-for i in $IMAGES; do
-	if [[ $i == *".$SNAPSHOT_SUFFIX" ]]; then
+for i in $IMAGES
+do
+	if [[ $i == *".$SNAPSHOT_SUFFIX" ]]
+        then
 		echo "Refusing to make a live backup of '$DOMAIN'!"
 		echo "The disk image '$i' appears to be a snapshot file while it should be a qcow2 base image."
 		echo "Making a snapshot of a snapshot is not a good idea and therefore the operation is aborted. At this point the intervention of a human is required."
@@ -68,12 +73,14 @@ virsh dumpxml "$DOMAIN" > "$BACKUP/$DOMAIN.xml"
 # Create the snapshot.
 #
 DISKSPEC=""
-for t in $TARGETS; do
+for t in $TARGETS
+do
     DISKSPEC="$DISKSPEC --diskspec $t,snapshot=external"
 done
 virsh snapshot-create-as --domain "$DOMAIN" --name $SNAPSHOT_SUFFIX --no-metadata \
 	--atomic --disk-only $DISKSPEC >/dev/null
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]
+then
     echo "Failed to create snapshot for $DOMAIN"
     exit 1
 fi
@@ -81,7 +88,8 @@ fi
 #
 # Copy disk images
 #
-for t in $IMAGES; do
+for t in $IMAGES
+do
     NAME=`basename "$t"`
     cp "$t" "$BACKUP"/"$NAME"
 done
@@ -90,18 +98,45 @@ done
 # Merge changes back.
 #
 BACKUPIMAGES=`virsh domblklist "$DOMAIN" --details | grep "^file[[:space:]]*disk" | awk '{print $4}'`
-for t in $TARGETS; do
-    virsh blockcommit "$DOMAIN" "$t" --active --pivot > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Could not merge changes for disk $t of $DOMAIN. VM may be in invalid state."
-        exit 1
-    fi
+for t in $TARGETS
+do
+    for retry_blockcommit in {1..5}
+    do
+        virsh blockcommit "$DOMAIN" "$t" --active --pivot > /dev/null
+        status=$?
+        if [ $? -eq 0 ]
+        then
+            # abort if all was fine
+            break
+        fi
+        # check if the job was retried too often
+        if [ $retry_blockcommit -eq 5 ]
+        then
+            echo "Error: Could not merge changes for disk $t of $DOMAIN after $retry_blockcommit retries. VM may be in an invalid state now."
+            echo ""
+            echo "I need the help of a human!"
+            exit 1
+        else
+            echo "Warning: Could not merge changes for disk $t of $DOMAIN on attempt #$retry_blockcommit, retrying in 5 seconds .."
+            sleep 5
+
+            virsh blockjob "$DOMAIN" "$t" --abort > /dev/null
+            if [ ! $? -eq 0 ]
+            then
+                echo "Error: Could not abort the blockjob after a failed blockcommit. VM may be in an invalid state now."
+                echo ""
+                echo "I need the help of a human!"
+                exit 1
+            fi
+        fi
+    done
 done
 
 #
 # Cleanup left over backup images.
 #
-for t in $BACKUPIMAGES; do
+for t in $BACKUPIMAGES
+do
     # echo "Cleaning up backup image $t"
     rm -f "$t"
 done
